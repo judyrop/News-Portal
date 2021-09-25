447dbc4e77691de56dd40960461a3c7b29ce5e46
Your Name <youremail@domain.com>
2021-09-25 16:22:30 +0300

    created test methods to our department class methods

---
+++ src/test/java/models/DepartmentTest.java
@@ -0,0 +1,54 @@
+package models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+//    @Test
+//      void  instantiatesCorrectly_true() {
+//
+//        assertEquals("true",testdepartment instanceof Department);
+//    }
+
+    @Test
+    void getDepartment_name() {
+        Department testdepartment = new Department("Software developers","section designed for users creating softwares of the company",20);
+        assertEquals("Software developers",testdepartment.getDepartment_name());
+    }
+
+    @Test
+    void setDepartment_name() {
+    }
+
+    @Test
+    void getDepartment_description() {
+        Department testdepartment = new Department("Software developers","section designed for users creating softwares of the company",20);
+        assertEquals("section designed for users creating softwares of the company",testdepartment.getDepartment_description());
+    }
+
+    @Test
+    void setDepartment_description() {
+    }
+
+    @Test
+    void getEmployees_number() {
+        Department testdepartment = new Department("Software developers","section designed for users creating softwares of the company",20);
+        assertEquals(20,testdepartment.getEmployees_number());
+    }
+
+    @Test
+    void setEmployees_number() {
+    }
+}
