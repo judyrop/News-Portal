4a4fb76a5f1927dc99076d888867b0214c7e0e2e
Your Name <youremail@domain.com>
2021-09-27 02:53:45 +0300

    started creating and working on app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,26 @@
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUserDao;
+import org.sql2o.Sql2o;
+
+import java.sql.Connection;
+
+import static spark.Spark.staticFileLocation;
+
 public class App {
+    public static void main(String[] args) {
+        Sql2oNewsDao newsDao;
+        Sql2oDepartmentDao departmentDao;
+        Sql2oUserDao userDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        staticFileLocation("/public");
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql' ";
+        Sql2o sql2o = new Sql2o(connectionString,"","");
+        newsDao = new Sql2oNewsDao(sql2o);
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        userDao = new Sql2oUserDao(sql2o);
+    }
 }
