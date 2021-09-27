5102a243d8d5f8801514616ba062c586e85549e0
Your Name <youremail@domain.com>
2021-09-27 03:10:09 +0300

    created routing to post news ,deartments and users

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,10 +2,14 @@ import com.google.gson.Gson;
 import dao.Sql2oDepartmentDao;
 import dao.Sql2oNewsDao;
 import dao.Sql2oUserDao;
+import models.Department;
+import models.News;
+import models.User;
 import org.sql2o.Sql2o;
 
 import java.sql.Connection;
 
+import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 
 public class App {
@@ -22,5 +26,29 @@ public class App {
         newsDao = new Sql2oNewsDao(sql2o);
         departmentDao = new Sql2oDepartmentDao(sql2o);
         userDao = new Sql2oUserDao(sql2o);
+        conn =  Sql2o.open();
+
+        post("/departments/:department_id/users/new", "application/json", (req, res) -> {
+            int department_id = Integer.parseInt(req.params("department_id"));
+            User user = gson.fromJson(req.body(), User.class);
+
+            user.setDepartment_id(department_id);
+            userDao.add(user);
+            res.status(201);
+            return gson.toJson(user);
+        });
+        post("/news/new", "application/json", (req, res) -> {
+            News news = gson.fromJson(req.body(), News.class);
+            newsDao.add(news);
+            res.status(201);
+            return gson.toJson(news);
+        });
+        post("/departments/new", "application/json", (req, res) -> {
+            Department department = gson.fromJson(req.body(), Department.class);
+            departmentDao.add(department);
+            res.status(201);
+            return gson.toJson(department);
+        });
+
     }
 }
